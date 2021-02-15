303fc93fc66a8e8e798c65e31d5ff8cb11414044
Vadim <nikitin.vl@phystech.edu>
2021-02-15 17:48:03 +0300

    Добавленая вторая часть задачи

--- main.cpp
+++ main.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
 #include <vector>
 
+void print_vector(const std::vector<int>& v);
+
 int main() {
 	std::vector<int> v(1, 0);
 	std::cout << "Currient size of vector = " << v.size() << std::endl;
 	std::cout << "Currient capacity of vector = " << v.capacity() << std::endl;
 	for (int i = 1; i < 20; ++i) {
-		std::cout << "Changing vector by using push_back...\n" << std::endl;
 		v.push_back(6);
-		std::cout << "Currient size of vector = " << v.size() << std::endl;
-		std::cout << "Currient capacity of vector = " << v.capacity() << std::endl;
+		print_vector(v);
 	} 
 	
 	//замечено, что ёмкость вектора увеличивается 2 в некотой степени элементов
 	//причём степень двойки увеличивается на единицу, начиная с 0
+
+	//***
 	
+	std::vector<int> s(1, 0);
+	s.reserve(5); //установили определённую ёмкость вектора, она в процессе работы не будет меняться
+	for (int i = 1; i < 5; ++i) {
+		s.push_back(6);
+		print_vector(s);
+	}
+	for (int i = 1; i < 5; ++i) {
+		s.pop_back();
+		print_vector(s);
+	}
 	return 0;
+}
+
+void print_vector(const std::vector<int>& v) {
+	std::cout << "SIZE = " << v.size() << ";   CAPASITY = " << v.capacity() << std::endl;
+	for (auto x : v)
+		std::cout << x << " ";
 }
