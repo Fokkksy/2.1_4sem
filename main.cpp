8f93c02728a0701c397e808b61c5bdc78bcaab8e
Vadim <nikitin.vl@phystech.edu>
2021-02-15 17:34:05 +0300

    Добавлен комментарий с ответом на вопрос

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <vector>
 
-
 int main() {
-	std::vector<int> v(5, 0);
+	std::vector<int> v(1, 0);
 	std::cout << "Currient size of vector = " << v.size() << std::endl;
 	std::cout << "Currient capacity of vector = " << v.capacity() << std::endl;
-	std::cout << "Changing vector by using push_back..." << std::endl;
-	v.push_back(6);
-	std::cout << "Currient size of vector = " << v.size() << std::endl;
-	std::cout << "Currient capacity of vector = " << v.capacity() << std::endl;
-	system("pause");
+	for (int i = 1; i < 20; ++i) {
+		std::cout << "Changing vector by using push_back...\n" << std::endl;
+		v.push_back(6);
+		std::cout << "Currient size of vector = " << v.size() << std::endl;
+		std::cout << "Currient capacity of vector = " << v.capacity() << std::endl;
+	} 
+	
+	//замечено, что ёмкость вектора увеличивается 2 в некотой степени элементов
+	//причём степень двойки увеличивается на единицу, начиная с 0
+	
 	return 0;
 }
