2abf75426b4dffcf31d696f9d5d9052dced0b0b6
Vadim <nikitin.vl@phystech.edu>
2021-02-15 17:25:07 +0300

    убрано лишнее

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <vector>
 
+const int N1 = 99999999999;
+
 int main() {
-	std::vector<int> v(20, 0);
+	std::vector<int> v(5, 0);
 	std::cout << "Currient size of vector = " << v.size() << std::endl;
 	std::cout << "Currient capacity of vector = " << v.capacity() << std::endl;
-	std::cout << "Changing elements of vector..." << std::endl;
-	for (auto x : v) {
-		x = 1234567890;
-	}
+	std::cout << "Changing vector by using push_back..." << std::endl;
+	v.push_back(6);
 	std::cout << "Currient size of vector = " << v.size() << std::endl;
 	std::cout << "Currient capacity of vector = " << v.capacity() << std::endl;
 	system("pause");
